eb47d0ef358ad5ec28f99465c41c8f4b138c05a7
williamc443 <57712080+williamc443@users.noreply.github.com>
2020-01-20 22:37:56 +0100

    Add files via upload

---
+++ level-1.js
@@ -0,0 +1,95 @@
+// Question 1
+
+var name;
+{
+  ("willam");
+}
+
+// Question 2
+
+var person = {
+  skill: "javascript"
+};
+
+// Question 3
+
+var outOfStock = false;
+if (outOfStock === false) {
+  console.log("In stock");
+} else {
+  console.log("Out of stock");
+}
+
+// Question 4
+
+var ages = [10, 20, 30, 40];
+
+for(var i = 0; i < ages.length; i++) {
+  console.log(ages[i]);
+
+// Question 5
+
+for (var count = 15; count <= 25; count++) {
+  console.log(count);
+}
+
+// Question 6
+
+for (var count = 15; count <= 25; count++) {
+  console.log((count = 20));
+}
+
+// Question 7
+
+var animals = [
+  // first animal
+  {
+    name: "lion",
+    feet: 4,
+    fatal: true
+  },
+  // second animal
+  {
+    name: "monkey",
+    feet: 2,
+    fatal: false
+  }
+];
+
+var animals = animals.length;
+console.log(animals);
+
+for (var i = 0; i < animals; i++) {
+  console.log(animals[i]);
+}
+
+// Question 8
+
+function  whatIDontLike(theWord) {
+  console.log("I don't like " + theWord);
+}
+
+whatIDontLike("fish balls");
+
+// Question 9
+
+function AddTwoNumbers(number1, number2) {
+  var sum = number1 - number2;
+  console.log(sum);
+}
+
+AddTwoNumbers(10, 4);
+
+// Question 10
+
+var learningJs = [];
+
+function array (elearningJs) {
+  console.log(learningJs)
+}
+learningJs[0] = "string"
+
+
+
+
+
